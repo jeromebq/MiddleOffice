33d0f5b9edd8566fb36d37a6f1c54fa24dfe4e45
BossXELA <alexandrehamoui@gmail.com>
2018-02-16 15:16:49 +0100

    Bisous

--- src/main/java/sparkexample/Questionnaire.java
+++ src/main/java/sparkexample/Questionnaire.java
@@ -1,6 +1,5 @@
 package sparkexample;
 
-import org.json.*;
 import static spark.Spark.*;
 
 import java.util.*;
@@ -11,115 +10,97 @@ public class Questionnaire {
 	static ArrayList<Demande> demandes = new ArrayList<Demande>();
 
 	public static void main(String[] args) {
-    	
-        setPort(80);
-        
-        get("/newdemande", (request, response) -> {
-        	
-        	return "<!DOCTYPE html>" +
-        	"<html>" +
-            "<head>" +
-            "<meta charset=\"utf-8\" />" +
-                "<title>Creation de demande</title>" +
-            "</head>" +
-        	"<body>" +
-        		"<p>Creation de demande : </p>" +
-        		"<form>"+
-        			"<p>"+
-        			"<label for=\"description\">Description :</label>"+
-        			"<input type=\"text\" name=\"description\" id=\"description\" />"+
-        			"<br />"+
-        			"<label for=\"chargeutile\">Charge utile :</label>"+
-        			"<input type=\"text\" name=\"chargeutile\" id=\"chargeutile\" />"+
-        			"<br />"+
-        			"<label for=\"urlsioui\">URL si oui :</label>"+
-        			"<input type=\"text\" name=\"urlsioui\" id=\"urlsioui\" />"+
-        			"<br />"+
-        			"<label for=\"urlsinon\">URL si non :</label>"+
-        			"<input type=\"text\" name=\"urlsinon\" id=\"urlsinon\" />"+
-        			"</p>"+
-        		"</form>"+
-        		"<form action=\"/demandes\">"+
-        			"<input type=\"submit\" value=\"Creer la demande\" />"+
-        		"</form>"+
-        		"<form action=\"/demandes\">"+
-        			"<input type=\"submit\" value=\"Annuler\" />"+
-        		"</form>"+
-            "</body>"+
-        	
-        "</html>";
-        });
 
-        post("/demandes", (request, response) -> {
-        	
-        	String id = UUID.randomUUID().toString();
-        	String question = request.queryParams("description");
-        	String chargeutile = request.queryParams("chargeutile");
-        	String ouiadresse = request.queryParams("urlsioui");
-        	String nonadresse = request.queryParams("urlsinon");
-        	
-        	demandes.add(new Demande(id, question, chargeutile, ouiadresse, nonadresse));
-        	
-        	response.redirect("/demandes");//Serveur redirect HTTP request Web client
-        	return "";
-        });
+		setPort(80);
 
-        get("/demandes", (request, response) -> {
-        	
-        	String affichage;
-        	
-        	affichage = "<!DOCTYPE html>"+
-        	"<html>"+
-        	  "<head>"+
-        	    "<title>Affichage des demandes</title>"+
-        	    "<meta charset=\"utf-8\">"+
-        	  "</head>"+
-        	  "<body>"+
-      		"<form action=\"/newdemande\">" +
-  			"<input type=\"submit\" value=\"Creer une demande\" />"+
-  			"</form>"+
-        	"<ul>";
-        	  for (Demande demande : demandes){
-        		  affichage += "<li><a href=\"/demandes/" + demande.getID() + "\">" + demande.getQuestion() + "</a></li>";
+		get("/newdemande", (request, response) -> {
 
-        	  }
-        	  affichage +="</ul>"+
-        	  "</body>"+
-        	"</html>";
-        	 return affichage;
-        });
-        
-        get("/demandes/:id", (request, response) -> {
-        	// Verifier si getVote =/ true sinon redirection sur les demandes
+			return "<!DOCTYPE html>" + "<html>" + "<head>" + "<meta charset=\"utf-8\" />"
+					+ "<title>Creation de demande</title>" + "</head>" + "<body>" + "<p>Creation de demande : </p>"
+					+ "<form method=\"post\" action=\"/demandes\">" + "<p>"
+					+ "<label for=\"description\">Description :</label>"
+					+ "<input type=\"text\" name=\"description\" id=\"description\" />" + "<br />"
+					+ "<label for=\"chargeutile\">Charge utile :</label>"
+					+ "<input type=\"text\" name=\"chargeutile\" id=\"chargeutile\" />" + "<br />"
+					+ "<label for=\"urlsioui\">URL si oui :</label>"
+					+ "<input type=\"text\" name=\"urlsioui\" id=\"urlsioui\" />" + "<br />"
+					+ "<label for=\"urlsinon\">URL si non :</label>"
+					+ "<input type=\"text\" name=\"urlsinon\" id=\"urlsinon\" />" + "</p>"
+					+ "<input type=\"submit\" value=\"Creer la demande\" />" + "</form>" + "<form action=\"/demandes\">"
+					+ "<input type=\"submit\" value=\"Annuler\" />" + "</form>" + "</body>" +
 
+					"</html>";
+		});
 
-        	return "html";
-        });
-        
-        post("/demandes/:id", (request, response) -> {
-        	// Verifier si getVote =/ true sinon redirection sur les demandes
-        	
-        	for (Demande demande : demandes){
-        		if (demande.getID().equals(request.params(":id")))
-        			if (request.queryParams("choix") == "oui") {
-        				demande.setVote();
-        				
-        			}
-        			else {
-        				
-        			}
-        	}
+		post("/demandes", (request, response) -> {
 
-        	
-        	return "<html>" +
-        			"<head>" +
-        			"<meta http-equiv=\"refresh\" content=\"1; URL=/demandes\">" +
-        			"</head>" +
-        			"<body>" +
-        			"</body>" +
-        			"</html>" +
-        			"<html>";
-        });
-       
-    }
+			String id = UUID.randomUUID().toString();
+			String question = request.queryParams("description");
+			String chargeutile = request.queryParams("chargeutile");
+			String ouiadresse = request.queryParams("urlsioui");
+			String nonadresse = request.queryParams("urlsinon");
+
+			demandes.add(new Demande(id, question, chargeutile, ouiadresse, nonadresse));
+
+			response.redirect("/demandes");
+
+			return "";
+		});
+
+		get("/demandes", (request, response) -> {
+
+			String affichage;
+
+			affichage = "<!DOCTYPE html>" + "<html>" + "<head>" + "<title>Affichage des demandes</title>"
+					+ "<meta charset=\"utf-8\">" + "</head>" + "<body>" + "<form action=\"/newdemande\">"
+					+ "<input type=\"submit\" value=\"Creer une demande\" />" + "</form>" + "<ul>";
+			for (Demande demande : demandes) {
+				affichage += "<li><a href=\"/demandes/" + demande.getID() + "\">" + demande.getQuestion() + "</a></li>";
+
+			}
+			affichage += "</ul>" + "</body>" + "</html>";
+			return affichage;
+		});
+
+		get("/demandes/:id", (request, response) -> {
+
+			Demande mydemande = null;
+
+			for (Demande demande : demandes) {
+				if (demande.getID().equals(request.params(":id")))
+					mydemande = demande;
+			}
+
+			String affichage;
+
+			if (!mydemande.getVote()) {
+				affichage = "<!DOCTYPE html>" + "<html>" + "<head>" + "<title>Affichage de la demande "
+						+ mydemande.getQuestion() + "</title>" + "<meta charset=\"utf-8\">" + "</head>" + "<body>"
+						+ "<form method=\"post\" action=\"/demandes/" + mydemande.getID() + "\">"
+						+ "<input type=\"submit\" value=\"Accepter la demande\" />" + "</form>"
+						+ "<form action=\"/demandes\">" + "<input type=\"submit\" value=\"Retour\" />" + "</form>"
+						+ "</body>";
+			} else {
+				affichage = "<!DOCTYPE html>" + "<html>" + "<head>" + "<title>Affichage de la demande "
+						+ mydemande.getQuestion() + "</title>"
+						+ "<meta http-equiv=\"refresh\" content=\"2; URL=/demandes\">" + "</head>" + "<body>"
+						+ "<a> Vous avez deja vote. La redirection est en cours. </a>" + "</body>" + "</html>" + "<html>";
+			}
+
+			return affichage;
+		});
+
+		post("/demandes/:id", (request, response) -> {
+
+			for (Demande demande : demandes) {
+				if (demande.getID().equals(request.params(":id")))
+					demande.setVote();
+			}
+
+			response.redirect("/demandes");
+
+			return "";
+		});
+
+	}
 }
